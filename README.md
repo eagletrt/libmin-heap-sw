b16d6f842cf177161330d9207697abb540c5d4b1
Dorijan Di Zepp <dorijan.dizepp@gmail.com>
2025-03-28 22:37:29 +0100

    doc: updated doc of library  #1

--- README.md
+++ README.md
@@ -1,36 +1,88 @@
-# LIBSTM32-SW-TEMPLATE
+# MIN-HEAP
 
-This repository serves as a template for libraries compatible with the
-[PlatformIO ecosystem](https://docs.platformio.org/en/latest/librarymanager/creating.html).
+This library implements all the functions used to create and manage a [minimum heap](https://it.wikipedia.org/wiki/Heap_(struttura_dati))
+data structure using a static array without dynamic memory allocation.
+
+> [!IMPORTANT]
+> This library does not guarantee that the functions will be executed correctly
+> if used with interrupts or multi-threaded architectures
+
+A min heap is a **partially ordered** binary tree-based data structure that guarantees that:
+1. The first element is always the minimum of all the elements
+2. Each element has a value always lower than the value of the children
+
+> [!TIP]
+> A *zero-indexed* array can be represented as a tree by choosing the first element as the root node
+> and the element at index $i * 2 + 1$ as the left child and $i * 2 + 2$ as the right child
+
+This library implements:
+- Insertion of an item in $O(log N)$ time complexity
+- Removal of any item (given the index in the heap) in $O(log N)$ time complexity
+- Search of any item in $O(N)$ time complexity
+
+> [!NOTE]
+> Removal of an item without the index requires a linear search of the array,
+> which implies linear time complexity (i.e. *no bueno*)
+
+## Dependencies
+This library uses [ArenaAllocator](https://github.com/eagletrt/libarena-allocator-sw.git) for memory management. Make sure to initialize ArenaAllocatorHandler_t to use prev_errors array, as shown in the following section.
 
 ## Usage
 
-Before starting to develop the library, a couple of things need to be done:
-1. Change this README explaining the library and the functionalities that it offers
-2. Modify the `library.json` including:
-    - The **name** of the library
-    - The library **version**
-    - The **description** explaining what the library does and for which devices
-    - The list of **keywords**
-    - The repository **url** (and type if necessary)
-    - The list of **authors**
-    - The supported **frameworks** and **platforms** (if needed)
-    - The list of **header files** of the library
-    - The list of **examples**
-    - The file of the library to **export** (if needed)
+To create a min heap first declare a variable using the `MinHeap` macro
+by providing the item type and the maximum number of element of the heap. \
+For example:
+```c
+MinHeap(int, 10) int_heap = ...;
+MinHeap(double, 7) double_heap = ...;
+MinHeap(struct Point, 2000) point_heap = ...;
+```
 
-## Structure
+Then initialize the heap using the `min_heap_new` macro that requires the same item type
+and capacity given in the declaration as well as a pointer to a function that should compare
+two items of the heap.<br/>
+An arena allocator, which must be initialized, will be needed to allocate the buffer required for storing the heap's data.
 
-The code of the library should be splitted in sources which must be placed inside
-the `src` folder and headers which must be placed inside the `include` folder.
+```c
+ArenaAllocatorHandler_t arena;
+arena_allocator_api_init(&arena);
+... = min_heap_new(int, 10, int_compare, &arena);
+... = min_heap_new(double, 7, double_compare, &arena);
+... = min_heap_new(struct Point, 2000, point_compare, &arena);
+```
 
-Inside the `example` folder multiple source files should be placed to further
-explain how to use the library and how it works in different scenario.
+The `min_heap_init` function can also be used to initialize the heap structure as follows:
+```c
+min_heap_init(&int_heap, int, 10, int_compare, &arena);
+min_heap_init(&double_heap, double, 7, double_compare, &arena);
+min_heap_init(&point_heap, struct Point, 2000, point_compare, &arena);
+```
 
-The library must be tested with the maximum possible code coverage, the source
-code used to run the unit tests should be put inside the `test` folder.
+The compare function signature is as follows:
+```c
+int8_t compare(void * a, void * b);
+```
 
-No other folders should be created besides the ones described before if not
-necessary, to handle complex file structures nested folders can be used.
+The two parameters `a` and `b` are the two items that should be compared and has to
+be casted to the type of the item used inside the heap. \
+The return value should be:
+- Negative(-1): if `a` is *less than* `b`
+- Zero(0): if `a` *equals* `b`
+- Positive(1): if `a` is *greater than* `b`
 
-For more info check the READMEs inside the corresponding folders.
+For example:
+```c
+int8_t int_compare(void * a, void * b) {
+    int f = *(int *)a;
+    int s = *(int *)b;
+    if (f < s) return -1;
+    return f == s ? 0 : 1;
+}
+```
+
+The `MinHeapReturnCode` enum is return by most of the functions of this library
+and **should always be checked** before attempting other operations with the data structure.
+
+## Examples
+
+For more info check the [examples](./examples/) folder.
--- include/min-heap.h
+++ include/min-heap.h
@@ -1,22 +1,33 @@
-/**
- * @file min-heap.h
- * @brief Library that implements a minimum heap with a static array without
- * dinamic memory allocation
+/*!
+ * \file min-heap.h
+ * \date 2025-03-28
+ * \authors Antonio Gelain [antonio.gelain@studenti.unitn.it]
+ * \authors Dorijan Di Zepp [dorijan.dizepp@eagletrt.it]
  *
- * @date 29 Feb 2024
- * @author Antonio Gelain [antonio.gelain@studenti.unitn.it]
+ * \brief Library that implements a minimum heap with a static array without
+ *      dinamic memory allocation
+ * 
+ * \details A min heap is a binary heap where the smallest element is 
+ *      always at the root. It maintains the heap property, meaning each 
+ *      parent node has a value less than or equal to its children. 
+ *      It is also a complete binary tree, meaning all levels are fully 
+ *      filled except possibly the last, which is filled from left to right. 
+ *      The main operations include insertion, deletion and top value. 
+ * 
+ * \warning The data buffer will not be deallocated automatically but has to be freed 
+ *      by using the arena allocator.
  */
 
- #ifndef MIN_HEAP_H
- #define MIN_HEAP_H
- 
- #include <stdint.h>
- #include <stddef.h>
- #include <stdbool.h>
- 
- /**
-  * @brief Type definition for the min heap structure
-  * @details As an example you can declare and initialize a new heap structure
+#ifndef MIN_HEAP_H
+#define MIN_HEAP_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+/*!
+  * \brief Type definition for the min heap structure
+  * \details As an example you can declare and initialize a new heap structure
   * that can contain 10 integers as follows:
   *      int8_t min_heap_compare(int a, int b) {
   *          if (a < b) return -1;
@@ -24,36 +35,36 @@
   *      }
   *      MinHeap(int, 10) heap = min_heap_new(int, 10, min_heap_compare);
   *
-  * @details The last item in the data array is used as swap storage when two items
+  * \details The last item in the data array is used as swap storage when two items
   * in the array needs to be swapped
   *
-  * @param TYPE The data type
-  * @param CAPACITY The maximum number of elements of the heap
+  * \param TYPE The data type
+  * \param CAPACITY The maximum number of elements of the heap
   *
-  * @param data_size The size a single element in bytes
-  * @param size The number of elements currently inside the heap
-  * @param capacity The maximum number of elements that heap can contain
-  * @param data The heap data
-  * @param compare A pointer to a function that compares two items and returns:
+  * \param data_size The size a single element in bytes
+  * \param size The number of elements currently inside the heap
+  * \param capacity The maximum number of elements that heap can contain
+  * \param data The heap data
+  * \param compare A pointer to a function that compares two items and returns:
   *     - -1: if the first item is less than the second
   *     -  0: if the items are equal
   *     -  1: if the first item is greater than the second
   */
- #define MinHeap(TYPE, CAPACITY) \
- struct { \
-     uint16_t data_size; \
-     size_t size; \
-     size_t capacity; \
-     int8_t (* compare)(void *, void *); \
-     TYPE data[CAPACITY + 1]; \
- }
- 
- /**
-  * @brief Min heap structure initialization
-  * @attention The TYPE and CAPACITY variables must be the same as the ones
+#define MinHeap(TYPE, CAPACITY)            \
+    struct {                               \
+        uint16_t data_size;                \
+        size_t size;                       \
+        size_t capacity;                   \
+        int8_t (*compare)(void *, void *); \
+        TYPE data[CAPACITY + 1];           \
+    }
+
+/*!
+  * \brief Min heap structure initialization
+  * \attention The TYPE and CAPACITY variables must be the same as the ones
   * in the structure declaration above
   *
-  * @details As an example you can declare and initialize a new heap structure
+  * \details As an example you can declare and initialize a new heap structure
   * that can contain 10 integers as follows:
   *      int8_t min_heap_compare(int a, int b) {
   *          if (a < b) return -1;
@@ -61,44 +72,44 @@
   *      }
   *      MinHeap(int, 10) heap = min_heap_new(int, 10, min_heap_compare);
   *
-  * @details If the min_heap_init function is used this macro is not needed
+  * \details If the min_heap_init function is used this macro is not needed
   *
-  * @param TYPE The data type
-  * @param CAPACITY The maximum number of elements of the heap
-  * @param CMP_CALLBACK The callback function used to compare items in the heap
+  * \param TYPE The data type
+  * \param CAPACITY The maximum number of elements of the heap
+  * \param CMP_CALLBACK The callback function used to compare items in the heap
   */
- #define min_heap_new(TYPE, CAPACITY, CMP_CALLBACK) \
- { \
-     .data_size = sizeof(TYPE), \
-     .size = 0, \
-     .capacity = CAPACITY, \
-     .compare = CMP_CALLBACK, \
-     .data = { 0 } \
- }
- 
- /**
-  * @brief Structure definition used to pass the heap handler as a function parameter
-  * @attention This structure should not be used directly
+#define min_heap_new(TYPE, CAPACITY, CMP_CALLBACK) \
+    {                                              \
+        .data_size = sizeof(TYPE),                 \
+        .size = 0,                                 \
+        .capacity = CAPACITY,                      \
+        .compare = CMP_CALLBACK,                   \
+        .data = { 0 }                              \
+    }
+
+/*!
+  * \brief Structure definition used to pass the heap handler as a function parameter
+  * \attention This structure should not be used directly
   */
- typedef struct {
-     uint16_t data_size;
-     size_t size;
-     size_t capacity;
-     int8_t (* compare)(void *, void *);
-     void * data;
- } MinHeapInterface;
- 
- /**
-  * @brief Enum with all the possible return codes for the min heap functions
+typedef struct {
+    uint16_t data_size;
+    size_t size;
+    size_t capacity;
+    int8_t (*compare)(void *, void *);
+    void *data;
+} MinHeapInterface;
+
+/*!
+  * \brief Enum with all the possible return codes for the min heap functions
   */
- typedef enum {
-     MIN_HEAP_OK,
-     MIN_HEAP_NULL_POINTER,
-     MIN_HEAP_EMPTY,
-     MIN_HEAP_FULL,
-     MIN_HEAP_OUT_OF_BOUNDS
- } MinHeapReturnCode;
- 
- typedef long signed_size_t;
+typedef enum {
+    MIN_HEAP_OK,
+    MIN_HEAP_NULL_POINTER,
+    MIN_HEAP_EMPTY,
+    MIN_HEAP_FULL,
+    MIN_HEAP_OUT_OF_BOUNDS
+} MinHeapReturnCode;
+
+typedef long signed_size_t;
 
 #endif
